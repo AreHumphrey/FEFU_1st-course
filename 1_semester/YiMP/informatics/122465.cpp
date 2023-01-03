2c9c16e0767f5437ad1d62903e84dab0b19b85ce
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:49:13 +1000

    Add files via upload

---
+++ 1_semester/YiMP/informatics/122465.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n, i, k, a[251], b, m;
+
+int main()
+{
+    cin >> n;
+    cin >> k;
+    i = -1;
+
+    while (k != 0)
+    {
+        i++;
+        a[i] = k % 10;
+        k = k / 10;
+    }
+
+    m = i;
+    b = a[m];
+    m--;
+
+    if (b < n)
+    {
+        b = b * 10 + a[m];
+        m--;
+    }
+
+    k = b % n;
+
+    while (m >= 0)
+    {
+        k = k * 10 + a[m];
+        m--;
+        k = k % n;
+    }
+
+    cout << k;
+}
