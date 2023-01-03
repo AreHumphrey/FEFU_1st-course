84c7030ff2d792db7b78cd044ba8f339bb410699
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:46:16 +1000

    Add files via upload

---
+++ 1_semester/YiMP/informatics/1587.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+
+
+using namespace std;
+
+int main()
+{
+    int DlinaPosl, x, x1, RusultProg = 0;
+    bool Flag = true, Srav = false;
+
+    ifstream InPutFail("input.txt");
+    ofstream OutPutFail("output.txt");
+
+    InPutFail >> DlinaPosl;
+    InPutFail>> x1;
+
+    for (int i = 1; i < DlinaPosl; i++) {
+        InPutFail >> x;
+        if (x != x1) {
+            if (Srav) {
+                if (Flag) {
+                    if (x > x1) {
+                        Flag = false;
+                    }
+                    else {
+                        RusultProg++;
+                    }
+                }
+                else {
+                    if (x < x1) {
+                        Flag = true;
+                    }
+                    else {
+                        RusultProg++;
+                    }
+                }
+            }
+            else {
+                Flag = x < x1;
+                Srav = true;
+            }
+        }
+        else {
+            RusultProg++;
+        }
+        x1 = x;
+    }
+    OutPutFail << RusultProg;
+    return 0;
+}
