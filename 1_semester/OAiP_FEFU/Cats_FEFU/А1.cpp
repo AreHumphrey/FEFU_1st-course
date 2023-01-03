288b033e7fdecf6e3362983f2dc812c3aa8943b2
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:37:52 +1000

    Add files via upload

---
+++ 1_semester/OAiP_FEFU/Cats_FEFU/А1.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    ifstream file1("input.txt");
+    ofstream file2("output.txt");
+    file1 >> a;
+    file1 >> b;
+
+    for (int i = 2; i < min(a, b); i++) {
+        while (a % i == 0 && b % i == 0) {
+            a /= i;
+            b /= i;
+        }
+    }
+    int steps2 = 0, steps5 = 0;
+    while (b % 2 == 0 || b % 5 == 0) {
+        if (b % 2 == 0) {
+            steps2++;
+            b /= 2;
+        }if (b % 5 == 0) {
+            steps5++;
+            b /= 5;
+
+        }
+    }
+
+
+    file2 << b << ' ' << max(steps2, steps5);
+    file2.close();
+    return 0;
+}
