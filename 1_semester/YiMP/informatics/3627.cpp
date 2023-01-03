d1fc938bf57a476a2b1c71df127ac047d9a7d038
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:48:12 +1000

    Add files via upload

---
+++ 1_semester/YiMP/informatics/3627.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+ 
+    int N = 0;
+    double X = 0, A = 0, Summa = 0;
+    cin >> N >> X;
+    cin >> A;
+    do {
+        Summa *= X;
+        Summa += A;
+        cin >> A;
+    } while (N--);
+    cout << Summa;
+    return 0;
+}
