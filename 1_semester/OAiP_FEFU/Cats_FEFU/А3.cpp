89c8aac9aff9813710f33430ec3aa9a7700ef013
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:39:04 +1000

    Add files via upload

---
+++ 1_semester/OAiP_FEFU/Cats_FEFU/А3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+	int KudCh, CountKudCh = 0;
+	ifstream InFail("input.txt");
+	ofstream OutFail("output.txt");
+	InFail >> KudCh;
+	for (int i = 1; i < KudCh; i++) {
+		int Chislo = i, PereverChislo = 0;
+		while (Chislo > 0) {
+			PereverChislo += Chislo % 10;
+			PereverChislo *= 10;
+			Chislo /= 10;}
+		PereverChislo /= 10;
+		if (PereverChislo + i == KudCh) {
+			CountKudCh += 1;}
+	}
+
+	OutFail << CountKudCh;
+	InFail.close();
+}
