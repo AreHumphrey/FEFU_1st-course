4bd8b548a6517e1b6f40805d185996e86b1519eb
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:19:30 +1000

    Add files via upload

---
+++ 1_semester/OAiP_FEFU/Informatics/3453.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    string st;
+    int N;
+    int Element;
+    set<int> ArrMass = {};
+
+    ifstream InPutFail("input.txt");
+    ofstream OutPutFail("output.txt");
+
+    InPutFail >> N;
+
+    for (int i = 0; i < N; i++) {
+        InPutFail >> st;
+        if (st != "COUNT") {
+            InPutFail >> Element;
+            if (st == "ADD") {
+                ArrMass.insert(Element);
+            }
+            else {
+                if (ArrMass.count(Element)) {
+                    OutPutFail << "YES" << "\n";
+                }
+                else {
+                    OutPutFail << "NO" << "\n";
+                }
+            }
+        }
+        else {
+            OutPutFail << ArrMass.size() << "\n";
+        }
+    }
+
+}
