22281ec4cb6dc8b75cf697bb5e65201d465392cd
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:41:31 +1000

    Add files via upload

---
+++ 1_semester/OAiP_FEFU/Cats_FEFU/B6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+	int NumberX, ReturnBitK, BitRezult;
+	fstream InFail("input.txt");
+	ofstream OutFail("output.txt");
+	InFail >> NumberX;
+	InFail >> ReturnBitK;
+	if (ReturnBitK > 31) {
+		OutFail << 0;
+	}
+	else {
+		BitRezult = (NumberX >> ReturnBitK) & 1;
+		OutFail << BitRezult;
+	}
+	
+	InFail.close(); OutFail.close();
+}
