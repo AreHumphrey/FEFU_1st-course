0d993c23740b88a966b963022657995f4ea1fc16
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:39:59 +1000

    Add files via upload

---
+++ 1_semester/OAiP_FEFU/Cats_FEFU/A7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+int main() {
+	int K, N, CountStrok, LineNumberInPage;
+	ifstream InFail("a.in");
+	ofstream OutFail("a.out");
+	InFail >> K >> N;
+
+	if (N % K != 0) {
+		LineNumberInPage = N / K + 1;
+		CountStrok = N - (N / K) * K;
+	}
+	else {
+		LineNumberInPage = N / K;
+		CountStrok = K;
+	}
+	OutFail << LineNumberInPage << " " << CountStrok;
+	InFail.close(); OutFail.close();
+}
