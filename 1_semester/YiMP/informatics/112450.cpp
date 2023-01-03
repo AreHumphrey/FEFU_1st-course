bd0ce96a6ee9b2cddce3c609e53a423bb06dcafb
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:48:43 +1000

    Add files via upload

---
+++ 1_semester/YiMP/informatics/112450.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int CountArrN, Index= 1, j, i; int* ArrN;
+	cin >> CountArrN;
+	ArrN = (int*)malloc(sizeof(int) * CountArrN);
+	ArrN[0] = 2;
+	for (i = 3; i <= CountArrN; i+=2) {
+		j = 3;
+		while ((j * j <= i) && (i % j != 0)) {
+			j += 2;
+		}
+		if (j * j > i) {
+			ArrN[Index] = i;
+			Index++;
+		}
+	}
+	for (int k = 0; k < Index; k++) {
+		cout << ArrN[k] << " ";
+		
+	}
+}
