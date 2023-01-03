2a1c0753b2f67289b16b302c8764f7849fdc25e5
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-03 22:49:55 +1000

    Add files via upload

---
+++ 1_semester/YiMP/informatics/1461.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string StringElem = "", ElementProject;
+    int CountElement, CurrenCount = 1, DelCount = 0, Index = -1; cin >> CountElement;
+    bool FlagElementPr = true;
+
+    for (int i = 0; i < CountElement; i++) {
+        cin >> ElementProject;
+        StringElem += ElementProject;
+    } StringElem += " "; CountElement = -1;
+
+    while (FlagElementPr) {
+        //cout << del_count << " <1> " << element_count << endl;/////
+
+        for (int i = 0; i < StringElem.length(); i++) {
+
+            if (StringElem[i] == StringElem[i + 1]) {
+                CurrenCount++;
+                if (Index == -1) { Index = i; }
+
+            }
+            else {
+
+                if (CurrenCount > 2) {
+                    DelCount += CurrenCount;
+                    StringElem.replace(Index, CurrenCount, "");
+
+                }
+                Index = -1;
+                CurrenCount = 1;
+            }
+        }
+        if (CountElement == DelCount) { FlagElementPr = false; }
+        else { CountElement = DelCount; }
+    }
+    cout << DelCount << endl;
+}
