7f95006c77ac475a0787ce09f69c305e7bfdecd1
Дина <115383388+humphrey116@users.noreply.github.com>
2023-01-20 01:02:21 +1000

    Add files via upload

---
+++ 1_semester/OAiP_FEFU/Laboratory_work_OAiP_FEFU/lab_5/Queue.h
@@ -0,0 +1,11 @@
+#ifndef QUEUE_QUEUE_H
+#define QUEUE_QUEUE_H
+
+struct stack *Init(int maxsize);
+int Delqueue();
+void EndQueue(int item);
+int Peek();
+int IsEmpty();
+int Deinitialization ();
+
+#endif //QUEUE_QUEUE_H
