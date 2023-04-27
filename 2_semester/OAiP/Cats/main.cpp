9a6d839c09c620118eef935f1dddb0ec9925f266
Dina <lutdina04@yandex.ru>
2023-04-28 01:27:44 +1000

    Add files via upload

--- 2_semester/OAiP/Cats/main.cpp
+++ 2_semester/OAiP/Cats/main.cpp
@@ -1,50 +1,83 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
-typedef long long ll;
-const ll inf = 3000000000;
-ll n, c, p, t, sum[105];
-ll check(ll mid) {
-    ll ans = 0, need = 0;
-    for (int i=1; i<=n; i++) {
-        ll tmp = sum[i], get = 0, need_time = 0;
-        if (tmp > mid) return -1;
-        tmp += need;
-        if (tmp > mid) {
-            need_time = (tmp + mid - 1) / mid * p;
-            get = mid;
-            need = tmp - mid;
-        } else {
-            need_time = (tmp > 0) ? p : 0;
-            get = tmp;
-            need = 0;
-        }
-        ans = max(ans, get);
-        t -= need_time;
-        if (t < 0) break;
-    }
-    return (t < 0) ? -1 : ans;
-}
+#include <fstream>
+#include <vector>
+
+struct Ticket {
+    int start;
+    int end;
+    int place;
+};
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0), cout.tie(0);
-    cin >> n >> c >> p >> t;
-    for (int i=1; i<=n; i++) {
-        ll x; cin >> x;
-        sum[i] = sum[i-1] + x;
+    std::ifstream input("trains.in");
+    std::ofstream output("trains.out");
+    int station, countTick, places;
+    input >> station >> countTick >> places;
+    std::vector<std::vector<Ticket>> tickets(station + 1); //station
+    for (int i = 0; i < countTick; i++) {
+        int start, end, place;
+        input >> start >> end >> place;
+        tickets[end].push_back(Ticket{start, end, place});
     }
-    ll ans = 0;
-    ll l = 0, r = inf;
-    while (l <= r) {
-        ll mid = (l + r) >> 1;
-        ll check_ans = check(mid);
-        if (check_ans != -1) {
-            ans = check_ans;
-            l = mid + 1;
-        } else {
-            r = mid - 1;
+    std::vector<int> answer(station + 1);{
+
+        std::vector<int> maxStation(places + 1, station);
+        std::vector<int> cntMax(station + 1, 0);
+        cntMax[station] = places;
+        int cntMaxIndex = station;
+        for (int i = station; i >= 1; i--) {
+            answer[i] = std::max(i, cntMaxIndex);
+            for (const Ticket &ticket: tickets[i]) {
+                cntMax[maxStation[ticket.place]]--;
+                maxStation[ticket.place] = ticket.start;
+                cntMax[maxStation[ticket.place]]++;
+            }
+            while (cntMax[cntMaxIndex] == 0) {
+                cntMaxIndex--;
+            }
+
+        }
+
+    }
+    const int MULTI_SIZE = 512;
+    std::vector<int> multiTicketMax(station + 1);
+    std::vector<int> multiTicketCount(station + 1);
+    for (int j = station; j >= 1; j--) {
+        if (answer[j] == j) {
+            multiTicketMax[j] = j;
+            multiTicketCount[j] = 0;
+        }
+        else {
+            int afterFirst = answer[j];
+            if (afterFirst / MULTI_SIZE != j / MULTI_SIZE) {
+                multiTicketMax[j] = afterFirst;
+                multiTicketCount[j] = 1;
+            }
+            else {
+                multiTicketMax[j] = multiTicketMax[afterFirst];
+                multiTicketCount[j] = multiTicketCount[afterFirst] + 1;
+            }
         }
     }
-    cout << ans << endl;
+    int q;
+    input >> q;
+    for (int i = 0; i < q; i++) {
+        int start, end;
+        input >> start >> end;
+        int count = 0;
+        while (multiTicketMax[start] > start && multiTicketMax[start] <= end) {
+            count += multiTicketCount[start];
+            start = multiTicketMax[start];
+        }
+        while (start < answer[start] && start < end) {
+            count++;
+            start = answer[start];
+        }
+        if (start < end) {
+            output << -1 << "\n";
+        }
+        else {
+            output << count << "\n";
+        }
+    }
     return 0;
 }
