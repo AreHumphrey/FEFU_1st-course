1cb411c560c73cb98f347f74653de3bc48c3c6c2
Dina <lutdina04@yandex.ru>
2023-06-18 11:35:12 +1000

    Update task3.cpp

--- 2_semester/OAiP/Laboratory_Work/Lab_3/task3.cpp
+++ 2_semester/OAiP/Laboratory_Work/Lab_3/task3.cpp
@@ -1,42 +1,77 @@
-#include "task3.h"
 #include <iostream>
-
-const int MAXN = 100;
-
+#include <algorithm>
+#include <vector>
+#include "task3.h"
+const int MAXN = 1000;
 int n;
-int a[MAXN][MAXN];
-int b[MAXN][MAXN];
+int a[MAXN + 1][MAXN + 1];
+int parent[MAXN + 1];
+int pInt[MAXN + 1];
+vector<pair<int, pair<int, int>>> edges;
 
-void floyd() {
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (a[i][k] + a[k][j] < a[i][j]) {
-                    a[i][j] = a[i][k] + a[k][j];
-                    b[i][j] = 1;
-                }
+void makeSet(int v) {
+    parent[v] = v;
+    pInt[v] = 0;
+}
+
+int findSet(int v) {
+    if (v == parent[v]) {
+        return v;
+    }
+    return parent[v] = findSet(parent[v]);
+}
+
+void unionSets(int a, int b) {
+    a = findSet(a);
+    b = findSet(b);
+    if (a != b) {
+        if (pInt[a] < pInt[b]) {
+            swap(a, b);
+        }
+        parent[b] = a;
+        if (pInt[a] == pInt[b]) {
+            pInt[a]++;
+        }
+    }
+}
+
+void kruskal() {
+    for (int i = 1; i <= n; i++) {
+        makeSet(i);
+        for (int j = i + 1; j <= n; j++) {
+            if (a[i][j] > 0) {
+                edges.push_back(make_pair(a[i][j], make_pair(i, j)));
             }
+        }
+    }
+    sort(edges.begin(), edges.end());
+    for (int i = 0; i < edges.size(); i++) {
+        int u = edges[i].second.first;
+        int v = edges[i].second.second;
+        int w = edges[i].first;
+        if (findSet(u) != findSet(v)) {
+            unionSets(u, v);
+            a[u][v] = a[v][u] = w;
         }
     }
 }
 
 void task3() {
-    std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            std::cin >> a[i][j];
-            if (a[i][j] != 0) {
-                b[i][j] = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    kruskal();
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (a[i][j] > 0) {
+                cout << "1 ";
+            } else {
+                cout << "0 ";
             }
         }
-    }
-    floyd();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            std::cout << b[i][j] << " ";
-        }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
-
-//алгоритм Флойда-Уоршелла для поиска кратчайших путей между всеми парами вершин в графе
